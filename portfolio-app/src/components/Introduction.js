b8740a098858ddf8f770cfc96711a8bf28de87bc
jammerwhy <david.yue@cru.org>
2019-06-06 16:58:59 -0400

    Added Components folder containing Introduction and Navbar

---
+++ portfolio-app/src/components/Introduction.js
@@ -0,0 +1,11 @@
+// This is the Introduction component
+
+import React, { Component } from "react";
+
+class Introduction extends Component {
+  render() {
+    return <p>Some text about who I am.</p>;
+  }
+}
+
+export default Introduction;
